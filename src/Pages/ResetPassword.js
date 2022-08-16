3b58e42a0a6b970a790a33ee2e543d66fcf7e5cb
mrsamia <15203060@iubat.edu>
2022-08-17 00:36:40 +0600

    SRM | IconSet

--- src/Pages/ResetPassword.js
+++ src/Pages/ResetPassword.js
@@ -3,29 +3,38 @@ import Button from '../Components/Input/Button';
 import Input from '../Components/Input/Input';
 import OR from '../Components/OR';
 import BlackTextButton from '../Components/BlackTextButton';
-import { RiRotateLockLine } from "react-icons/ri";
+import { VscLock } from "react-icons/vsc";
 function ResetPassword(props) {
     return (
         <div className='container'>
             <div className='formWrapper pad restPassformWrapper'>
-                <RiRotateLockLine className='Iconsize'/>
-                <div>
-                    <p className='TrbleLogIn'>Trouble Logging In?</p>
+                <div className='cardpading'>
+                    <div className='d-flex justify-content-center'>
+                    <div className='IconWraper'>
+                        <VscLock className='Iconsize' />
+                    </div>
+                    </div>
+                    <div>
+                        <p className='TrbleLogIn'>Trouble Logging In?</p>
+                    </div>
+                    <div>
+                        <p className='txt_epu'>
+                            Enter your email, phone, or username and we'll send you a link to get back into your account.
+                        </p>
+                    </div>
+                    <Input name="MNE" placeholder='Email Phone or UserName' />
+                    <Button className="loginBtn" name="Send Login Link" />
+                    <div className='pt-3'>
+                        <OR />
+                    </div>
+                    <BlackTextButton className='createNewAccnt' name="Create New Account" />
                 </div>
-                <div>
-                    <p className='txt_epu'>
-                    Enter your email, phone, or username and we'll send you a link to get back into your account.
-                    </p>
-                </div>
-                <Input name="MNE" placeholder='Email Phone or UserName' />
-                <Button className="loginBtn" name="Send Login Link" />
-                <OR />
-                <BlackTextButton className='createNewAccnt' name="Create New Account"/>
+
                 <div className='BackToLoginWrapper'>
-                <BlackTextButton className="BackToLogin" name="Back To Login"/>
+                    <BlackTextButton className="BackToLogin" name="Back To Login" />
                 </div>
-      
             </div>
+
         </div>
     );
 }
