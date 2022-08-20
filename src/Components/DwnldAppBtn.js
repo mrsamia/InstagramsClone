97e43f795db0c94b5f264811550004db86745861
mrsamia <15203060@iubat.edu>
2022-08-20 20:46:31 +0600

    SRM | Update

--- src/Components/DwnldAppBtn.js
+++ src/Components/DwnldAppBtn.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function DwnldAppBtn(props) {
     return (
         <div className='Img'>
-            <img className='StoreImg' src={require('./images/apple.png')} />
-            <img className='StoreImg' src={require('./images/google.png')} />
+           <Link to={"https://apps.apple.com/app/instagram/id389801252?vt=lo"}> <img className='StoreImg' src={require('./images/apple.png')} /></Link>
+           <Link to={"https://play.google.com/store/apps/details?id=com.instagram.android"}><img className='StoreImg' src={require('./images/google.png')} /></Link>
         </div>
     );
 }
